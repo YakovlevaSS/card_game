8f48657a261b56dd2bbe529ef716b5f9cb763a6f
Sofia Iakovleva <sofyakovleva95@gmail.com>
2023-07-30 00:37:09 +0300

    Рендер формы выбора и "поймана" сложность

---
+++ component/renderPlayingField.js
@@ -0,0 +1,10 @@
+export function renderPlayingField(levelPoint, appEl) {
+    const appHtml = `
+    <div class="wrap center">
+    <div class="game-field">
+    <h2 class="game-field__title">${levelPoint.value} карточек
+    </h2>
+    </div>
+    </div>`;
+    appEl.innerHTML = appHtml;
+}
