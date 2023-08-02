217a033dea4b768ebab9a9584cf7149c460e6b8a
Sofia Iakovleva <sofyakovleva95@gmail.com>
2023-08-02 11:31:50 +0300

    добавлена верстка страницы 2

--- component/renderPlayingField.js
+++ component/renderPlayingField.js
@@ -1,21 +1,32 @@
-import { cardArray } from "./cardArray.js"
+import { cardArray } from './cardArray.js'
 
 export function renderPlayingField(levelPoint, appEl) {
-    
-    let level = levelPoint.value;
+    let level = levelPoint.value
 
-    const coverCardArr = [];
-    for (let index = 0; index < level; index++) {
-        coverCardArr.push(`<img src="./img/рубашка.png">`)
-        console.log(coverCardArr);        
+    const coverCardArr = []
+    function getCoverArr() {
+        for (let index = 0; index < level; index++) {
+            coverCardArr.push(`<img src="./img/рубашка.png">`)
+        }
+        console.log(coverCardArr)
+        return coverCardArr
     }
-    
+    getCoverArr()
+
     const appHtml = `
     <div class="wrap">
-    <div class="game-field">
-    <h2 class="game-field__title">${levelPoint.value} карточек
-    </h2>
-    </div>
-    </div>`
+    <header class="center-big">
+      <div class="timer ">
+        <div class="timer__units">
+            <p class="timer__min">min</p>
+            <p class="timer__sek">sek</p>
+        </div>
+        <div class="timer__time">00.00</div>
+      </div>  
+      <button type="submit" class="button" id="button-new-start" >Начать заново</button>
+    </header>
+    <div class="game-field center-big">${coverCardArr}</div>
+</div>
+`
     appEl.innerHTML = appHtml
 }
