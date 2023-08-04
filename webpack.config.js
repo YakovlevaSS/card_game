04b9d8efc51a5452a4ac27bcfbe4807e21138b79
Sofia Iakovleva <sofyakovleva95@gmail.com>
2023-08-04 17:03:36 +0300

    установлены и настроены лоудеры, указана точка входа и выхода

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,24 @@
+const path = require('path')
 module.exports = {
-    entry: './index.js', // Входной файл, в котором мы пишем свой код
-    output: {
-        filename: 'main.js', // Выходной файл, который подключаем к HTML
-        // Обратите внимание, сохранится он по пути "./dist/main.js"
+    entry: './index.js',
+
+    module: {
+        rules: [
+          { test: /\.css$/, use: ['style-loader','css-loader'] },
+          { test: /\.scss$/, use: ["style-loader", "scss-loader"] },
+          {
+            test: /\.(png|svg|jpg|jpeg|gif)$/i,
+            type: "asset/resource",
+          },
+          {
+            test: /\.(woff|woff2|eot|ttf|otf)$/i,
+            type: "asset/resource",
+          },
+        ]
+      },
+      output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "main.js",
+        clean: true,
     },
-}
+    }
