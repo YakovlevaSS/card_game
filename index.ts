d3fcdedfb3a669175283a12177463da95048d50d
YakovlevaSS <sofyakovleva95@gmail.com>
2023-08-17 15:17:36 +0300

    исправлены  переменные

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import './style.css'
 import { renderChoicePage } from './component/renderChoicePage'
 
-let appEl: Element | null = document.querySelector('.app')
+const appEl: Element | null = document.querySelector('.app')
 renderChoicePage(appEl)
